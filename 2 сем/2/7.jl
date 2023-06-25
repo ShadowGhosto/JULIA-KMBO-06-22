c4598ae03d9b93c486584866ecc241518979ad8c
ShadowGhosto <115074349+ShadowGhosto@users.noreply.github.com>
2023-06-25 13:09:32 +0300

    1курс

--- 2 сем/2/7.jl
+++ 2 сем/2/7.jl
@@ -1,9 +1,9 @@
-function newton_method(f, df, x0, tol, max_iter)
+function Newton_(f, df, x0, tol, max_i)
     x = x0
-    iter = 0
-    while abs(f(x)) > tol && iter < max_iter
+    i = 0
+    while abs(f(x)) > tol && i < max_i
         x -= f(x) / df(x)
-        iter += 1
+        i += 1
     end
     return x
 end
