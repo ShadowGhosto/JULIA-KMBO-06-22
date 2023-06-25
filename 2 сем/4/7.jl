b6e3db118aaea7fbb52efb6a2f72d0b8a0232b94
ShadowGhosto <115074349+ShadowGhosto@users.noreply.github.com>
2023-06-25 13:20:29 +0300

    1курс

--- 2 сем/4/7.jl
+++ 2 сем/4/7.jl
@@ -1,6 +1,5 @@
-# Производительный Гаусс
 using LinearAlgebra
-function gaussian_elimination(A)
+function Gaussian_Elim(A)
     n = size(A, 1)
 
     for k = 1:n-1
@@ -14,7 +13,5 @@ function gaussian_elimination(A)
 end
 
 
-
-
 A = rand(Float64, (1000, 1000))
-@time gaussian_elimination(A)
+@time Gaussian_Elim(A)
